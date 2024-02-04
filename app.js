8305fc8de3ea4b3d0db6828829d0295a81de5e7d
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-04 15:39:11 +0600

    adding

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const http = require('http') //imported
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit(); // server er event loop ke exit kore and program shut down kore
-
-})
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+    res.write('</html>');
+});
 server.listen(3000); //any port ami chaile use korte parbu
