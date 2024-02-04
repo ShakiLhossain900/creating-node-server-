ba57007816f98d48cbe274183b6875cad091689e
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-04 15:59:34 +0600

    adding

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const http = require('http') //imported
+// const http = require('http') //imported
 
-//create server
-// function rqListener (req, res){
-// }
-// http.createServer(rqListener);
+// //create server
+// // function rqListener (req, res){
+// // }
+// // http.createServer(rqListener);
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit(); // server er event loop ke exit kore and program shut down kore
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-    res.write('</html>');
+// const server = http.createServer((req,res) => {
+//     console.log(req.url, req.method, req.headers);
+//     // process.exit(); // server er event loop ke exit kore and program shut down kore
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page</title></head>');
+//     res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+//     res.write('</html>');
+// });
+// server.listen(3000); //any port ami chaile use korte parbu
+
+//ROUTING REQUEST
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
 });
 server.listen(3000); //any port ami chaile use korte parbu
