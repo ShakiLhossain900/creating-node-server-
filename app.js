d302d0f4e5b6bc58669145c109b1642f491cfb9a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-02-04 15:32:50 +0600

    adding

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const http = require('http') //imported
 // http.createServer(rqListener);
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
     // process.exit(); // server er event loop ke exit kore and program shut down kore
-    
+
 })
 server.listen(3000); //any port ami chaile use korte parbu
